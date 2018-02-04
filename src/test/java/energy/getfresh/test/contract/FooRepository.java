2f315af92e061bf5d6f20add2cd302521c095277
Kazik Pogoda <kazik.pogoda@getfresh.energy>
2018-02-05 00:10:06 +0100

    old test app code removed

--- src/test/java/energy/getfresh/test/contract/FooRepository.java
+++
@@ -1,9 +0,0 @@
-package energy.getfresh.test.contract;
-
-import org.springframework.data.repository.CrudRepository;
-
-/**
- * @author morisil
- */
-public interface FooRepository extends CrudRepository<Foo, Long> {
-}
