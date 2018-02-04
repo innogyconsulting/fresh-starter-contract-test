51c47d204f24766a3df0a41ccf188ef3f5b16077
Kazik Pogoda <kazik.pogoda@getfresh.energy>
2018-02-05 00:12:20 +0100

    test cases and README

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
 # fresh-starter-contract-test
 Spring Boot extensions for convenient testing of API contracts
+
+# Example
+
+```groovy
+@RunWith(SpringRunner)
+@SpringBootTest(
+    classes = UserApiApplication,
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+class UserApiContractTest {
+
+  @Test
+  void 'GET /users/{id} for existent user id: should return 1 element result'() {
+    //given
+    User user = api.populate(new User(
+        name: 'John Smith'
+    ))
+
+    given()
+        .contentType('application/json')
+
+        .when()
+        .get("/users/${user.id}")
+
+        .then()
+        .statusCode(200)
+        .body(jsonEquals("""
+        {
+          "name" : "John Smith",
+          "_links" : {
+            "self" : {
+              "href" : "http://localhost:${api.port}/users/${user.id}"
+            },
+            "user" : {
+              "href" : "http://localhost:${api.port}/users/${user.id}"
+            }
+          }
+        }
+        """))
+  }
+
+  @Inject
+  @Rule
+  public ApiContext api
+
+}
+```
+
+For more examples see ......
+
+# Features
+
+* BDD style with source code following natural language
+* API test cases can be shared as common language between all the roles in the team (dev, qa, product)
+* Test case description in natural language
+* full container test instead of mock mvc
+* full JSON in assertions
+  * access to all the variables
+* All the features of RestAssured TODO link library
+* Easy population of DB state before the test thanks to groovy default constructors
+* Auto-cleanup of populated entities after each test
+* DB population and cleanup does not interfere with transaction spawned by API call
+* minimal dependency on ApiContext test rule
+  * separate instance of ApiContext injected before each test, compatible with concurrent test execution
+* Informative error messages
+  * HTTP request/response trace only on error
+  * JSON 
--- src/test/groovy/energy/getfresh/test/contract/UserApiContractTest.groovy
+++ src/test/groovy/energy/getfresh/test/contract/UserApiContractTest.groovy
@@ -1,8 +1,23 @@
+/*
+ * Copyright 2018 https://getfresh.energy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package energy.getfresh.test.contract
 
-import energy.getfresh.test.contract.testapp.FreshApiContractTestApplication
-import energy.getfresh.test.contract.testapp.UserRepository
 import energy.getfresh.test.contract.testapp.User
+import energy.getfresh.test.contract.testapp.UserApiApplication
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -16,140 +31,302 @@ import static io.restassured.RestAssured.given
 import static org.assertj.core.api.Assertions.assertThat
 
 /**
- *
+ * API contract test for test application exposing {@link User}
+ * as REST resource.
  *
  * @author morisil
  */
 @RunWith(SpringRunner)
 @SpringBootTest(
-    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-    classes = FreshApiContractTestApplication
+    classes = UserApiApplication,
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
-class FooApiContractTest {
-
-  @Inject @Rule public TestContext context
-
-  @Inject UserRepository fooRepository
+class UserApiContractTest {
 
   @Test
-  void 'POST /customers with valid customer data: should write customer in DB'() {
-    String customerLink = given()
+  void 'POST /users with valid user data: should write user in DB'() {
+    given()
         .contentType('application/json')
         .body('''
         {
-          "name": "John",
-          "surname": "Smith",
-          "birthDate": "1981-12-18T00:00"
+          "name": "John Smith"
         }
         ''')
 
         .when()
-        .post('/customers')
+        .post('/users')
 
         .then()
         .statusCode(201) // created
-        .extract()
-        .path('_links.self.href')
+        .body(api.trackPersistedEntity(User))
+        .body(jsonEquals("""
+        {
+          "name" : "John Smith",
+          "_links" : {
+            "self" : {
+              "href" : "http://localhost:${api.port}/users/${api.persistedEntity.id}"
+            },
+            "user" : {
+              "href" : "http://localhost:${api.port}/users/${api.persistedEntity.id}"
+            }
+          }
+        }
+        """))
 
     // then
-    Long customerId = BelcantoTests.extractId(customerLink)
-    assertThat(fooRepository.findById(customerId))
-        .isPresent()
-        .hasValueSatisfying { customer ->
-      assertThat(customer.name).isEqualTo('John')
-      assertThat(customer.surname).isEqualTo('Smith')
-      assertThat(customer.birthDate).isEqualTo('1981-12-18')
-    }
+    User user = api.getPersistedEntity()
+    assertThat(user).isNotNull()
+    assertThat(user.name).isEqualTo('John Smith')
   }
 
   @Test
-  void 'POST /customers with embedded address: should save customer and address in db'() {
-    String customerLink = given()
+  void 'POST /users with invalid JSON: should return error'() {
+    given()
         .contentType('application/json')
         .body('''
         {
-          "name": "John",
-          "surname": "Smith",
-          "addresses": [
-            {
-              "street": "Somewhere"
-            }            
-          ]
+          "name": "Mistake
         }
         ''')
 
         .when()
-        .post('/customers')
+        .post('/users')
 
         .then()
-        .statusCode(201) // created
-        .extract()
-        .path('_links.self.href')
-
-    // then
-    Long customerId = BelcantoTests.extractId(customerLink)
-    assertThat(customerRepository.findById(customerId))
-        .isPresent()
-        .hasValueSatisfying { customer ->
-      assertThat(customer.name).isEqualTo('John')
-      assertThat(customer.surname).isEqualTo('Smith')
-      assertThat(customer.addresses)
-          .hasSize(1)
-          .hasOnlyOneElementSatisfying { address ->
-        assertThat(address.street).isEqualTo('Somewhere')
-      }
-    }
+        .statusCode(400)
   }
 
   @Test
-  void 'GET /foo/{id} for non-existent id: should return 404'() {
+  void 'GET /users for non-existent users: should return empty result'() {
     given()
-        .pathParam('nonExistentId', 42L)
+        .contentType('application/json')
 
         .when()
-        .get('/customers/{nonExistentId}')
-
-        .then()
-        .statusCode(404) // not found
-  }
-
-  @Test
-  void 'GET /foo/id for existing foo: should return foo data'() {
-    // given
-    // db state
-    def foo = context.populate(
-        new User( // convenient way of populating DB state with Groovy
-            bar: 'buzz'
-        )
-    )
-    given()
-
-        .when()
-        .get("/customers/${customer.id}")
+        .get('/users')
 
         .then()
         .statusCode(200)
-        .content(jsonEquals("""
+        .body(jsonEquals("""
         {
-          "name" : "John",
-          "surname" : "Smith",
-          "addresses" : [ ],
-          "appointments" : [ ],
-          "birthDate" : null,
-          "displayName" : "John Smith",
+          "_embedded" : {
+            "users" : [ ]
+          },
           "_links" : {
             "self" : {
-              "href" : "http://localhost:${context.port}/customers/${customer.id}"
+              "href" : "http://localhost:${api.port}/users{?page,size,sort}",
+              "templated" : true
             },
-            "customer" : {
-              "href" : "http://localhost:${context.port}/customers/${customer.id}"
+            "profile" : {
+              "href" : "http://localhost:${api.port}/profile/users"
+            }
+          },
+          "page" : {
+            "size" : 20,
+            "totalElements" : 0,
+            "totalPages" : 0,
+            "number" : 0
+          }
+        }
+        """))
+  }
+
+  @Test
+  void 'GET /users for 2 existent users: should return 2 element result'() {
+    //given
+    User user1 = api.populate(new User(
+        name: 'John Smith'
+    ))
+    User user2 = api.populate(new User(
+        name: 'Jan Musterman'
+    ))
+
+    given()
+        .contentType('application/json')
+
+        .when()
+        .get('/users')
+
+        .then()
+        .statusCode(200)
+        .body(jsonEquals("""
+        {
+          "_embedded" : {
+            "users" : [ {
+              "name" : "John Smith",
+              "_links" : {
+                "self" : {
+                  "href" : "http://localhost:${api.port}/users/${user1.id}"
+                },
+                "user" : {
+                  "href" : "http://localhost:${api.port}/users/${user1.id}"
+                }
+              }
             },
-            "specialist" : {
-              "href" : "http://localhost:${context.port}/customers/${customer.id}/specialist"
+            {
+              "name" : "Jan Musterman",
+              "_links" : {
+                "self" : {
+                  "href" : "http://localhost:${api.port}/users/${user2.id}"
+                },
+                "user" : {
+                  "href" : "http://localhost:${api.port}/users/${user2.id}"
+                }
+              }
+            } ]
+          },
+          "_links" : {
+            "self" : {
+              "href" : "http://localhost:${api.port}/users{?page,size,sort}",
+              "templated" : true
+            },
+            "profile" : {
+              "href" : "http://localhost:${api.port}/profile/users"
+            }
+          },
+          "page" : {
+            "size" : 20,
+            "totalElements" : 2,
+            "totalPages" : 1,
+            "number" : 0
+          }
+        }
+        """))
+  }
+
+  @Test
+  void 'GET /users for 2 existent users and 1 element per page: should return 1 element with link to the next page'() {
+    //given
+    User user1 = api.populate(new User(
+        name: 'John Smith'
+    ))
+    api.populate(new User(
+        name: 'Jan Musterman'
+    ))
+
+    given()
+        .contentType('application/json')
+
+        .when()
+        .get('/users?size=1')
+
+        .then()
+        .statusCode(200)
+        .body(jsonEquals("""
+        {
+          "_embedded" : {
+            "users" : [ {
+              "name" : "John Smith",
+              "_links" : {
+                "self" : {
+                  "href" : "http://localhost:${api.port}/users/${user1.id}"
+                },
+                "user" : {
+                  "href" : "http://localhost:${api.port}/users/${user1.id}"
+                }
+              }
+            } ]
+          },
+          "_links" : {
+            "first" : {
+              "href" : "http://localhost:${api.port}/users?page=0&size=1"
+            },
+            "self" : {
+              "href" : "http://localhost:${api.port}/users{&sort}",
+              "templated" : true
+            },
+            "next" : {
+              "href" : "http://localhost:${api.port}/users?page=1&size=1"
+            },
+            "last" : {
+              "href" : "http://localhost:${api.port}/users?page=1&size=1"
+            },
+            "profile" : {
+              "href" : "http://localhost:${api.port}/profile/users"
+            }
+          },
+          "page" : {
+            "size" : 1,
+            "totalElements" : 2,
+            "totalPages" : 2,
+            "number" : 0
+          }
+        }
+        """))
+  }
+
+  @Test
+  void 'GET /users/{id} for existent user id: should return 1 element result'() {
+    //given
+    User user = api.populate(new User(
+        name: 'John Smith'
+    ))
+
+    given()
+        .contentType('application/json')
+
+        .when()
+        .get("/users/${user.id}")
+
+        .then()
+        .statusCode(200)
+        .body(jsonEquals("""
+        {
+          "name" : "John Smith",
+          "_links" : {
+            "self" : {
+              "href" : "http://localhost:${api.port}/users/${user.id}"
+            },
+            "user" : {
+              "href" : "http://localhost:${api.port}/users/${user.id}"
             }
           }
         }
         """))
   }
+
+  @Test
+  void 'GET /users/{id} for non-existent user id: should return 404'() {
+    given()
+        .contentType('application/json')
+
+        .when()
+        .get('/users/666')
+
+        .then()
+        .statusCode(404)
+  }
+
+  @Test
+  void 'DELETE /users/{id} for existent user id: should delete'() {
+    //given
+    User user = api.populate(new User(
+        name: 'John Smith'
+    ))
+
+    given()
+        .contentType('application/json')
+
+        .when()
+        .delete("/users/${user.id}")
+
+        .then()
+        .statusCode(204)
+  }
+
+  @Test
+  void 'DELETE /users/{id} for non-existent user id: should return 404'() {
+    given()
+        .contentType('application/json')
+
+        .when()
+        .delete('/users/666')
+
+        .then()
+        .statusCode(404)
+  }
+
+  @Inject
+  @Rule
+  public ApiContext api
 
 }
